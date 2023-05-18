ca58e54ca9f723bb84427ee66ce32549e577531e
Andreas Reif <andreas@nysics.com>
2023-05-17 22:30:40 -0600

    created demo module to get Notion content

---
+++ modules/page-properties.js
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router';
+import { UrlParams } from '../dependencies/context';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+export default function PageProperties({}) {
+	let urlParams = useContext(UrlParams);
+
+	let {
+		id,
+		key
+	} = urlParams;
+
+	let [data, setData] = useState({});
+
+	useEffect(() => {
+		if (!id) return;
+		axios.get('https://notion-embed-tools.nysics.workers.dev/databases/' + id).then((r) =>{
+			setData(r);
+		})
+	}, [urlParams])
+
+	return(<>
+		URL Key: {key ? key : 'Loading key..'}<br/>
+		URL ID: {id ? id : 'Loading ID...'}<br/>
+		Data: <pre>{JSON.stringify(data, null, '\t')}</pre>
+	</>);
+}
