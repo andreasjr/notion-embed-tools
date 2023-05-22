d0012a629e3cd951e4b443fbefe67937c4120122
Andreas Reif <andreas@nysics.com>
2023-05-22 13:12:18 -0600

    Add browser mode detection

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,19 @@
 import '../styles/globals.scss';
+import {
+	useEffect
+} from 'react';
+
 export default function App({ Component, pageProps }) {
+	function inIframe () {
+		try {
+			console.log(window)
+			return window.self !== window.top;
+		} catch (e) {
+			return true;
+		}
+	}
+	useEffect(() => {
+		if (!inIframe()) document.body.classList.add('browser-mode');
+	});
 	return <Component {...pageProps} />;
 }
